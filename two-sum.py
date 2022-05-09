087745fa0526612899fd46b87ac291e57fe4b70e
Josh Herndon <68031344+Josh-Herndon@users.noreply.github.com>
2022-05-09 08:55:38 -0500

    Update two-sum.py

--- two-sum.py
+++ two-sum.py
@@ -1,8 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        for i in range(len(nums)):
-            pair_member = target - nums[i]
-            if pair_member in nums[i+1:]:
-                index = nums[i+1:].index(pair_member) + len(nums[:i+1])
-                return [i, index]
+        idx_map = {}
+        
+        for i, val in enumerate(nums):
+            num = target - val
+            
+            if num in idx_map:
+                return [i, idx_map[num]]
+            
+            idx_map[val] = i
