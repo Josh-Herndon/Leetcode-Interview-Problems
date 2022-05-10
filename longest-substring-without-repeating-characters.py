ea2a83da941d6a67ffae2907730f9ff5f94bd803
Josh Herndon <68031344+Josh-Herndon@users.noreply.github.com>
2022-05-10 15:08:30 -0500

    Update longest-substring-without-repeating-characters.py

--- longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters.py
@@ -16,3 +16,26 @@ class Solution:
             arr.append(letter)
             
         return max(maximum, len(arr))
+    
+## Solution 2
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+        idx_map = {}
+        low, current, maximum, last = 0, 0, 0, -1
+        
+        for i, letter in enumerate(s):
+            if letter in idx_map:
+                current = i - low
+                new_pos = idx_map[letter] + 1
+                
+                low = new_pos if new_pos > low else low
+                
+            if current > maximum:
+                maximum = current
+                
+            last = i
+            idx_map[letter] = i
+            
+        return max(maximum, last - low + 1)
